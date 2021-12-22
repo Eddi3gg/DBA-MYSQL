1e98b137e8a4bbce4b958d7c2c2d346a6a57f680
Eddi3gg <96434904+Eddi3gg@users.noreply.github.com>
2021-12-22 15:05:38 -0300

    Inserção de dados

---
+++ Inserção de dados.sql
@@ -0,0 +1,33 @@
+
+insert into clientes (nome, sobrenome, endereco)
+values 
+			( 'Edson' , 'Oliver' , 'Rua sete de setembro' ),
+		( 'Caroline' , 'Lamberti' , 'Rua Uruguaiana' ),
+		( 'Beatriz' , 'Ferraz' , 'Rua de Osasco' ),
+		( 'Gustavo' , 'Atomic' , 'Rua sete de Guarulhos' ),
+        ( 'Giovana' , 'Ribeiro' , 'Rua da mooca' ), 
+         ( 'Arthur' , 'Alemao' , 'Rua Rio Grande do Sul' ) ;       
+         
+       
+
+         
+insert into marcas ( nome_marca, origem )
+ values 					
+				   ('Mitsubish', 'Toquio'),
+                   ('Jeep', 'Alemanha'),
+                   ('Honda', 'Japao'),
+                   ('Toyota', 'Japao'),
+                   ('volkswagen', 'Alemanha'),
+                   ('Ford', 'EUA');
+
+                   
+                    insert into inventario ( modelo, transmissão, motor, combustivel)
+ values 
+						('Lancer EVOLUTION X' ,' TURBO INTERCOOLER' , '4X4 16V' ,' GASOLINA 4P AUTOMÁTICO' , '1' ),
+						( 'RENEGADE , 4P AUTOMÁTICO' , '1.8 16V FLEX' , 'Gasolina e álcool' , '2' ),
+                        ( 'CIVIC ', ' 4P AUTOMÁTICO' ,'1.8 LXL 16V FLEX ' , 'Gasolina e álcool' , '3' ),
+                        ( 'COROLLA' , '4P AUTOMÁTICO ', '2.0 XEI 16V FLEX ', 'Gasolina e álcool' , '4' ),
+                        ( 'FIT, 4P AUTOMÁTICO' , '	1.4 LX 16V FLEX' , 'Gasolina e álcool', '3' ),
+						( 'GOLF' ,' 4P TIPTRONIC ',' 2.0 MI SPORTLINE 8V FLEX ' ,' Gasolina e álcool' ,'5' ),
+                        ('FUSION' ,'  4P AUTOMÁTICO', '2.0 SEL 16V' ,' Gasolina' , '6' );
+					
